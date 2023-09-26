94b68d71e22519d74a7175cfb0f11454486452ec
Pradip Maiti <pradip.maiti555@gmail.com>
2023-09-26 22:36:51 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+// document.querySelector('#app').innerHTML = `
+// <h1>heading 1</h1><h2>
+// `
+ 
+
+// setupCounter(document.querySelector('#counter'))
---
+++ readme.md
@@ -0,0 +1,6 @@
+# cmd command 
+
+- to create project : `yarn create vite|npm create vite`
+- enter details your project: project name,project module- vanilla, language-javascript 
+- open that project folder in cmd and then `yarn|npm install`
+- to start the project `yarn dev|npm run dev` 
